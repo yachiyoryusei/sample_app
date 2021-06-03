c46b2f4940be3a05e02aea3a27f93015ae0add19
Ubuntu <ubuntu@ip-172-31-9-196.ap-northeast-1.compute.internal>
2021-06-03 07:27:14 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
 class StaticPagesController < ApplicationController
+  
   def home
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
-  def helf
+  def help
   end
   
   def about
